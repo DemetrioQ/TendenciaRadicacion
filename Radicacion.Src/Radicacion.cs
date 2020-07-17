bbaf9b3373bdd41e93f4c2b2d4a64f17cded13e4
DemetrioQ <52639107+DemetrioQ@users.noreply.github.com>
2020-07-16 20:33:44 -0400

    first commit

---
+++ Radicacion.Src/Radicacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Radicacion.Src
+{
+    class Radicacion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
